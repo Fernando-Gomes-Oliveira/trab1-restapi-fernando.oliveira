9a14544792c655302147f338ef14375c00c188bb
Fernando Oliveira <fernando.oliveira@ipvc.pt>
2025-07-02 18:34:34 +0100

    resolvidoa

--- backend/routes/cursos.js
+++ backend/routes/cursos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ctrl = require('../controllers/cursoController');
+const ctrl = require('../controllers/cursosController');
 
 // Apenas leitura de cursos (GET)
 router.get('/', ctrl.listar);
